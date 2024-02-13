5b91349b54782756a7632391f63bba4def060363
Ichwan Rizky <ichwanrizky.info@gmail.com>
2024-02-13 11:05:31 +0700

    init commit, feat: setup login, sessions, and roles

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,221 @@
+import { NextResponse } from "next/server";
+import prisma from "@/libs/db";
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.formData();
+    const username = body.get("username")?.toString();
+    const password = body.get("password")?.toString();
+
+    // empty username & password
+    if (!username || !password) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Missing username and password",
+        }),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    // get data by username
+    const getData = await prisma.user.findFirst({
+      where: {
+        username: username,
+      },
+      include: {
+        roles: {
+          include: {
+            access_menu: {
+              include: {
+                menu: {
+                  select: {
+                    path: true,
+                  },
+                },
+              },
+              orderBy: [
+                {
+                  menu: {
+                    menu_group: {
+                      urut: "asc",
+                    },
+                  },
+                },
+                {
+                  menu: {
+                    urut: "asc",
+                  },
+                },
+              ],
+              take: 1,
+            },
+          },
+        },
+      },
+    });
+
+    // invalid username
+    if (!getData) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Invalid username or password",
+        }),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    // check password
+    const checkPassword = await bcrypt.compare(password, getData.password);
+
+    // invalid password
+    if (!checkPassword) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Invalid username or password",
+        }),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const path = getData.roles?.access_menu?.[0]?.menu?.path;
+    if (!path) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    // new json
+    const newDataUser = {
+      id: getData.id,
+      username: getData.username,
+      roleId: getData.roles?.id,
+      roleName: getData.roles?.role_name,
+      path: path,
+    };
+
+    // generate token
+    const token = await jwt.sign({ data: newDataUser }, process.env.JWT, {
+      expiresIn: "1h",
+    });
+
+    // format date
+    const currendDate = new Date();
+    const formattedDate = new Date(currendDate);
+    formattedDate.setHours(formattedDate.getHours() + 7);
+
+    // expires date
+    const expiredDate = new Date(formattedDate);
+    expiredDate.setHours(expiredDate.getHours() + 1);
+
+    // insert session
+    const session = await prisma.$transaction(async (prisma) => {
+      const existSession = await prisma.session.findFirst({
+        where: {
+          user_id: getData.id,
+          expiredAt: {
+            gte: formattedDate,
+          },
+          expired: false,
+        },
+      });
+
+      if (!existSession) {
+        return await prisma.session.create({
+          data: {
+            user_id: getData.id,
+            token: token,
+            createdAt: formattedDate,
+            expiredAt: expiredDate,
+          },
+        });
+      } else {
+        return await prisma.session.update({
+          data: {
+            token: token,
+            expiredAt: expiredDate,
+          },
+          where: {
+            id: existSession.id,
+          },
+        });
+      }
+    });
+
+    if (!session) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Something went wrong",
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        status: true,
+        message: "Login success",
+        data: {
+          ...newDataUser,
+          accessToken: token,
+        },
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: error.message,
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+  }
+}
---
+++ src/libs/authOptions.tsx
@@ -0,0 +1,97 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+    // 1h
+    maxAge: 60 * 60,
+  },
+  secret: process.env.JWT,
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      type: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        const { username, password } = credentials as {
+          username: string;
+          password: string;
+        };
+
+        const body = new FormData();
+        body.append("username", username);
+        body.append("password", password);
+
+        const response = await fetch(
+          process.env.NEXT_PUBLIC_API_URL + "/api/auth/login",
+          {
+            method: "POST",
+            body: body,
+          }
+        );
+
+        const dataUser = await response.json();
+
+        if (dataUser.status) {
+          return dataUser.data;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile, user }: any) {
+      if (account?.provider === "credentials") {
+        token.id = user.id;
+        token.username = user.username;
+        token.roleId = user.roleId;
+        token.roleName = user.roleName;
+        token.path = user.path;
+        token.accessToken = user.accessToken;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }: any) {
+      delete session.user.email;
+      delete session.user.name;
+      delete session.user.image;
+
+      if ("id" in token) {
+        session.user.id = token.id;
+      }
+
+      if ("username" in token) {
+        session.user.username = token.username;
+      }
+
+      if ("roleId" in token) {
+        session.user.roleId = token.roleId;
+      }
+
+      if ("roleName" in token) {
+        session.user.roleName = token.roleName;
+      }
+
+      if ("path" in token) {
+        session.user.path = token.path;
+      }
+
+      if ("accessToken" in token) {
+        session.user.accessToken = token.accessToken;
+      }
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+};
