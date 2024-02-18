6e767b8f74d0f7a0ef56cce99e423028b9bfe860
Ichwan Rizky <ichwanrizky.info@gmail.com>
2024-02-19 00:30:31 +0700

    add db

--- dev_absendulu2024.sql
+++ dev_absendulu2024.sql
@@ -11,11 +11,32 @@
  Target Server Version : 100427 (10.4.27-MariaDB)
  File Encoding         : 65001
 
- Date: 13/02/2024 16:04:32
+ Date: 19/02/2024 00:28:06
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for access_department
+-- ----------------------------
+DROP TABLE IF EXISTS `access_department`;
+CREATE TABLE `access_department`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `department_id` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `access_department_role_id_fkey`(`role_id` ASC) USING BTREE,
+  INDEX `access_department_department_id_fkey`(`department_id` ASC) USING BTREE,
+  CONSTRAINT `access_department_department_id_fkey` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `access_department_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of access_department
+-- ----------------------------
+INSERT INTO `access_department` VALUES (7, 1, 2);
+INSERT INTO `access_department` VALUES (8, 1, 6);
 
 -- ----------------------------
 -- Table structure for access_menu
@@ -24,21 +45,44 @@ DROP TABLE IF EXISTS `access_menu`;
 CREATE TABLE `access_menu`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `menu_id` int NULL DEFAULT NULL,
-  `role_id` int NULL DEFAULT NULL,
   `action` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `role_id` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `access_menu_menu_id_fkey`(`menu_id` ASC) USING BTREE,
+  UNIQUE INDEX `access_menu_menu_id_role_id_key`(`menu_id` ASC, `role_id` ASC) USING BTREE,
   INDEX `access_menu_role_id_fkey`(`role_id` ASC) USING BTREE,
   CONSTRAINT `access_menu_menu_id_fkey` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `access_menu_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+  CONSTRAINT `access_menu_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 69 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of access_menu
 -- ----------------------------
-INSERT INTO `access_menu` VALUES (1, 1, 1, NULL);
-INSERT INTO `access_menu` VALUES (2, 2, 1, 'insert,update,delete');
-INSERT INTO `access_menu` VALUES (3, 4, 1, 'insert,update,delete');
+INSERT INTO `access_menu` VALUES (63, 1, 'insert,update,delete,view', 1);
+INSERT INTO `access_menu` VALUES (64, 2, 'insert,update,delete,view', 1);
+INSERT INTO `access_menu` VALUES (65, 3, 'insert,update,delete,view', 1);
+INSERT INTO `access_menu` VALUES (66, 4, 'insert,update,delete,view', 1);
+INSERT INTO `access_menu` VALUES (67, 5, 'view,update,insert,delete', 1);
+INSERT INTO `access_menu` VALUES (68, 6, 'view,insert,update,delete', 1);
+
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_department` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lot` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `latitude` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `longitude` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `radius` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+INSERT INTO `department` VALUES (2, 'panji jaya', '', '1.0411609836065656', '104.00322731118862', '0.08');
+INSERT INTO `department` VALUES (6, 'dummy', '', '', '', '');
 
 -- ----------------------------
 -- Table structure for menu
@@ -58,9 +102,12 @@ CREATE TABLE `menu`  (
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
-INSERT INTO `menu` VALUES (1, 'department', '/master_data/department', 1, 1);
-INSERT INTO `menu` VALUES (2, 'menu group', '/configuration/menugroup', 1, 2);
-INSERT INTO `menu` VALUES (4, 'menu', '/configuration/menu', 2, 2);
+INSERT INTO `menu` VALUES (1, 'menu group', '/configuration/menugroup', 1, 1);
+INSERT INTO `menu` VALUES (2, 'menu', '/configuration/menu', 2, 1);
+INSERT INTO `menu` VALUES (3, 'roles', '/configuration/roles', 3, 1);
+INSERT INTO `menu` VALUES (4, 'access', '/configuration/access', 4, 1);
+INSERT INTO `menu` VALUES (5, 'department', '/masterdata/department', 1, 2);
+INSERT INTO `menu` VALUES (6, 'sub department', '/masterdata/subdepartment', 2, 2);
 
 -- ----------------------------
 -- Table structure for menu_group
@@ -74,13 +121,13 @@ CREATE TABLE `menu_group`  (
   `parent_id` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `menu_group_menu_group_key`(`menu_group` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of menu_group
 -- ----------------------------
-INSERT INTO `menu_group` VALUES (1, 'master data', 1, 1, 'master_data');
-INSERT INTO `menu_group` VALUES (2, 'configuration', 99, 1, 'configuration');
+INSERT INTO `menu_group` VALUES (1, 'configuration', 2, 1, 'configuration');
+INSERT INTO `menu_group` VALUES (2, 'master data', 1, 1, 'masterdata');
 
 -- ----------------------------
 -- Table structure for roles
@@ -89,13 +136,15 @@ DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `role_name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `roles_role_name_key`(`role_name` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of roles
 -- ----------------------------
 INSERT INTO `roles` VALUES (1, 'administrator');
+INSERT INTO `roles` VALUES (2, 'test');
 
 -- ----------------------------
 -- Table structure for session
@@ -109,19 +158,34 @@ CREATE TABLE `session`  (
   `expiredAt` datetime NULL DEFAULT NULL,
   `expired` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `Session_token_key`(`token` ASC) USING BTREE,
-  INDEX `Session_user_id_fkey`(`user_id` ASC) USING BTREE,
-  UNIQUE INDEX `Session_token_user_id_key`(`token` ASC, `user_id` ASC) USING BTREE,
-  CONSTRAINT `Session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `session_token_key`(`token` ASC) USING BTREE,
+  UNIQUE INDEX `session_user_id_token_key`(`user_id` ASC, `token` ASC) USING BTREE,
+  CONSTRAINT `session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of session
 -- ----------------------------
-INSERT INTO `session` VALUES (8, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImljaHdhbiIsInJvbGVJZCI6MSwicm9sZU5hbWUiOiJhZG1pbmlzdHJhdG9yIiwicGF0aCI6Ii9tYXN0ZXJkYXRhL2RlcGFydG1lbnQifSwiaWF0IjoxNzA3Nzk4OTgwLCJleHAiOjE3MDc4MDI1ODB9.2V1t2t0Eja-S8eG5ljU57sg0S0Mqdgkhqi8B0im_CPg', 1, '2024-02-13 10:55:56', '2024-02-13 12:36:20', 0);
-INSERT INTO `session` VALUES (9, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImljaHdhbiIsInJvbGVJZCI6MSwicm9sZU5hbWUiOiJhZG1pbmlzdHJhdG9yIiwicGF0aCI6Ii9tYXN0ZXJkYXRhL2RlcGFydG1lbnQifSwiaWF0IjoxNzA3ODAyNzMxLCJleHAiOjE3MDc4MDYzMzF9.v9QVTCi9BLtuceIiwGQtzMX4c9bj1q_x8gaSHNUuNu4', 1, '2024-02-13 12:38:04', '2024-02-13 13:38:51', 0);
-INSERT INTO `session` VALUES (10, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImljaHdhbiIsInJvbGVJZCI6MSwicm9sZU5hbWUiOiJhZG1pbmlzdHJhdG9yIiwicGF0aCI6Ii9tYXN0ZXJkYXRhL2RlcGFydG1lbnQifSwiaWF0IjoxNzA3ODA4MjEwLCJleHAiOjE3MDc4MTE4MTB9.fT9dt6c_kCs-bggoxLvBkfUennGMb6YNl-SXSU0-5Kc', 1, '2024-02-13 14:10:10', '2024-02-13 15:10:10', 0);
-INSERT INTO `session` VALUES (11, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImljaHdhbiIsInJvbGVJZCI6MSwicm9sZU5hbWUiOiJhZG1pbmlzdHJhdG9yIiwicGF0aCI6Ii9tYXN0ZXJkYXRhL2RlcGFydG1lbnQifSwiaWF0IjoxNzA3ODEyMTc3LCJleHAiOjE3MDc4MTU3Nzd9.uPOFqvDPx7YxwBBC3ssAL-05Ev2oRXyEaHYujIwbsHk', 1, '2024-02-13 15:10:20', '2024-02-13 16:16:17', 0);
+INSERT INTO `session` VALUES (1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo1LCJ1c2VybmFtZSI6ImljaHdhbiIsInJvbGVJZCI6MSwicm9sZU5hbWUiOiJhZG1pbmlzdHJhdG9yIiwicGF0aCI6Ii9tYXN0ZXJkYXRhL2RlcGFydG1lbnQifSwiaWF0IjoxNzA4MjY0MTE3LCJleHAiOjE3MDgyNjc3MTd9.BFWhgdm6nSX5VQzTcNIHU1ptmq7Hx7Xjb75CayHOHYg', 5, '2024-02-18 20:48:37', '2024-02-18 21:48:37', 0);
+INSERT INTO `session` VALUES (2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo1LCJ1c2VybmFtZSI6ImljaHdhbiIsInJvbGVJZCI6MSwicm9sZU5hbWUiOiJhZG1pbmlzdHJhdG9yIiwicGF0aCI6Ii9tYXN0ZXJkYXRhL2RlcGFydG1lbnQifSwiaWF0IjoxNzA4MjY4NjMxLCJleHAiOjE3MDgyNzIyMzF9.Ulguz9EEUeEIKbroSW3QvnTtrA6PwhmP8a8jT6FAGws', 5, '2024-02-18 22:03:51', '2024-02-18 23:03:51', 0);
+
+-- ----------------------------
+-- Table structure for sub_department
+-- ----------------------------
+DROP TABLE IF EXISTS `sub_department`;
+CREATE TABLE `sub_department`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_sub_department` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `department_id` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `sub_department_department_id_fkey`(`department_id` ASC) USING BTREE,
+  CONSTRAINT `sub_department_department_id_fkey` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sub_department
+-- ----------------------------
+INSERT INTO `sub_department` VALUES (1, 'management', 2);
 
 -- ----------------------------
 -- Table structure for user
@@ -137,11 +201,11 @@ CREATE TABLE `user`  (
   UNIQUE INDEX `user_username_key`(`username` ASC) USING BTREE,
   INDEX `user_rolesId_fkey`(`rolesId` ASC) USING BTREE,
   CONSTRAINT `user_rolesId_fkey` FOREIGN KEY (`rolesId`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'ichwan', '$2b$10$VX6nw/u0wslLQ7El6NB5E.y4sOxfUxlh0YT6g6EVd6uGF6NIr4ygq', '2024-02-13 10:10:01', 1);
+INSERT INTO `user` VALUES (5, 'ichwan', '$2b$10$Tv9V18ztZ1EfBAKA9J/4cOdow0PsKO6ucIypw55Aai7nK36IworI.', '2024-02-17 14:23:10', 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
