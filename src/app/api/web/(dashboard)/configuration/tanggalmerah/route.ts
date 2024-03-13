023691d8c3390efd555e7c87375bd97ffe51aab0
Ichwan Rizky <ichwanrizky.info@gmail.com>
2024-03-13 21:42:49 +0700

    feat: API get & create tanggalmerah

---
+++ src/app/api/web/(dashboard)/configuration/tanggalmerah/route.ts
@@ -0,0 +1,278 @@
+import { NextResponse } from "next/server";
+import { checkSession } from "@/libs/checkSession";
+import { checkRoles } from "@/libs/checkRoles";
+import prisma from "@/libs/db";
+
+export async function GET(req: Request) {
+  try {
+    const authorization = req.headers.get("Authorization");
+
+    const session = await checkSession(authorization);
+    if (!session[0]) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const roleId = session[1].roleId;
+    const roleAccess = await checkRoles(roleId, "/configuration/tanggalmerah");
+    if (!roleAccess) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const actions = roleAccess?.action ? roleAccess?.action.split(",") : [];
+    if (!actions.includes("view")) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const data = await prisma.tanggal_merah.findMany({
+      include: {
+        tanggal_merah_list: {
+          select: {
+            tanggal: true,
+          },
+        },
+      },
+      orderBy: [
+        {
+          tahun: "desc",
+        },
+        {
+          bulan: "asc",
+        },
+      ],
+    });
+
+    if (!data) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Data not found",
+        }),
+        {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        status: true,
+        message: "success",
+        data: data,
+        actions: actions,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error?.name == "TokenExpiredError") {
+        return new NextResponse(
+          JSON.stringify({
+            status: false,
+            message: "Session Expired",
+          }),
+          {
+            status: 401,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: error.name,
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const authorization = req.headers.get("Authorization");
+
+    const session = await checkSession(authorization);
+    if (!session[0]) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const roleId = session[1].roleId;
+    const roleAccess = await checkRoles(roleId, "/configuration/tanggalmerah");
+    if (!roleAccess) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const actions = roleAccess?.action ? roleAccess?.action.split(",") : [];
+    if (!actions.includes("insert")) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const body = await req.formData();
+    const department = body.get("department")!.toString();
+    const bulan = body.get("bulan")!.toString();
+    const tahun = body.get("tahun")!.toString();
+    const tanggal_merah = body.get("tanggal_merah");
+
+    const parseTanggalMerah = tanggal_merah
+      ? JSON.parse(tanggal_merah as string)
+      : [];
+
+    const create = await prisma.tanggal_merah.create({
+      data: {
+        department_id: Number(department),
+        tahun: Number(tahun),
+        bulan: Number(bulan),
+        tanggal_merah_list: {
+          create: parseTanggalMerah.map((item: any) => ({
+            tanggal: new Date(
+              `${tahun}-${bulan.padStart(2, "0")}-${item.value
+                .toString()
+                .padStart(2, "0")}`
+            ),
+          })),
+        },
+      },
+    });
+
+    if (!create) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Failed to create tanggal merah",
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        status: true,
+        message: "Success to create tanggal merah",
+        data: create,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error?.name == "TokenExpiredError") {
+        return new NextResponse(
+          JSON.stringify({
+            status: false,
+            message: "Session Expired",
+          }),
+          {
+            status: 401,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: error.name,
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+  }
+}
