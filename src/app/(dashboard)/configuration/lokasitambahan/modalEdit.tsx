8380cdd57e0d49df38f6235906adef603cef916f
Ichwan Rizky <ichwanrizky.info@gmail.com>
2024-05-13 16:52:14 +0700

    feat: menu lokasi tambahan

---
+++ src/app/(dashboard)/configuration/lokasitambahan/modalEdit.tsx
@@ -0,0 +1,182 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+type Props = {
+  isModalOpen: any;
+  onClose: any;
+  accessToken?: string;
+  data: Location;
+};
+
+type Location = {
+  id: number;
+  lokasi: string;
+  latitude: string;
+  longitude: string;
+  radius: string;
+};
+
+const ModalEdit = (props: Props) => {
+  const { isModalOpen, onClose, accessToken, data } = props;
+
+  const pathname = usePathname();
+  const lastSlashIndex = pathname.lastIndexOf("/");
+  const menu_url = pathname.substring(lastSlashIndex + 1);
+
+  // loading state
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [lokasi, setLokasi] = useState(data.lokasi);
+  const [latitude, setLatitude] = useState(data.latitude);
+  const [longitude, setLongitude] = useState(data.longitude);
+  const [radius, setRadius] = useState(data.radius);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (confirm("Edit this data?")) {
+      setIsLoading(true);
+      try {
+        const body = new FormData();
+        body.append("lokasi", lokasi);
+        body.append("latitude", latitude);
+        body.append("longitude", longitude);
+        body.append("radius", radius);
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/web/lokasitambahan/${data.id}?menu_url=${menu_url}`,
+          {
+            method: "POST",
+            headers: {
+              authorization: `Bearer ${accessToken}`,
+            },
+            body: body,
+          }
+        );
+
+        const res = await response.json();
+        if (!response.ok) {
+          alert(res.message);
+        } else {
+          alert(res.message);
+          onClose();
+        }
+      } catch (error) {
+        alert("something went wrong");
+      }
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    isModalOpen && (
+      <>
+        <div
+          className="modal-backdrop fade show"
+          style={{ display: "block" }}
+        ></div>
+        <div
+          className="modal fade show"
+          tabIndex={-1}
+          role="dialog"
+          style={{ display: "block" }}
+        >
+          <div className="modal-dialog">
+            <form onSubmit={handleSubmit}>
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5 fw-semibold">
+                    EDIT LOCATION
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    onClick={onClose}
+                    aria-label="Close"
+                  />
+                </div>
+                <div className="modal-body">
+                  <div className="form-group mb-3">
+                    <label className="mb-1 fw-semibold small">LOCATION</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      style={{ textTransform: "uppercase" }}
+                      onChange={(e) => setLokasi(e.target.value)}
+                      value={lokasi}
+                      required
+                    />
+                  </div>
+
+                  <div className="form-group mb-3">
+                    <label className="mb-1 fw-semibold small">LATITUDE</label>
+                    <input
+                      type="number"
+                      step={"any"}
+                      className="form-control"
+                      onChange={(e) => setLatitude(e.target.value)}
+                      value={latitude}
+                      required
+                    />
+                  </div>
+
+                  <div className="form-group mb-3">
+                    <label className="mb-1 fw-semibold small">LONGITUDE</label>
+                    <input
+                      type="number"
+                      step={"any"}
+                      className="form-control"
+                      onChange={(e) => setLongitude(e.target.value)}
+                      value={longitude}
+                      required
+                    />
+                  </div>
+
+                  <div className="form-group mb-3">
+                    <label className="mb-1 fw-semibold small">RADIUS (m)</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      onChange={(e) => setRadius(e.target.value)}
+                      value={radius}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-dark btn-sm"
+                    onClick={onClose}
+                  >
+                    CLOSE
+                  </button>
+                  {isLoading ? (
+                    <button
+                      type="button"
+                      className="btn btn-primary btn-sm"
+                      disabled
+                    >
+                      <span
+                        className="spinner-border spinner-border-sm me-2"
+                        role="status"
+                        aria-hidden="true"
+                      ></span>
+                      LOADING...
+                    </button>
+                  ) : (
+                    <button type="submit" className="btn btn-primary btn-sm">
+                      SAVE CHANGES
+                    </button>
+                  )}
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </>
+    )
+  );
+};
+
+export default ModalEdit;
---
+++ src/app/api/web/(config)/lokasitambahan/[id]/route.ts
@@ -0,0 +1,384 @@
+import { NextResponse } from "next/server";
+import { checkSession } from "@/libs/checkSession";
+import { checkRoles } from "@/libs/checkRoles";
+import prisma from "@/libs/db";
+import { handleError } from "@/libs/handleError";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const authorization = req.headers.get("Authorization");
+
+    const session = await checkSession(authorization);
+    if (!session[0]) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const searchParams = new URL(req.url).searchParams;
+    const menu_url = searchParams.get("menu_url");
+    if (!menu_url) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const roleId = session[1].roleId;
+    const roleAccess = await checkRoles(roleId, menu_url);
+    if (!roleAccess) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const id = params.id;
+    if (!id) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Data not found",
+        }),
+        {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const data = await prisma.lokasi_tambahan.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!data) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Data not found",
+        }),
+        {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        status: true,
+        message: "success",
+        data: data,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    return handleError(error);
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const authorization = req.headers.get("Authorization");
+
+    const session = await checkSession(authorization);
+    if (!session[0]) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const searchParams = new URL(req.url).searchParams;
+    const menu_url = searchParams.get("menu_url");
+    if (!menu_url) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const roleId = session[1].roleId;
+    const roleAccess = await checkRoles(roleId, menu_url);
+    if (!roleAccess) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const actions = roleAccess?.action ? roleAccess?.action.split(",") : [];
+    if (!actions.includes("update")) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const id = params.id;
+    if (!id) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Data not found",
+        }),
+        {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+    const body = await req.formData();
+    const lokasi = body.get("lokasi")!.toString();
+    const latitude = body.get("latitude")!.toString();
+    const longitude = body.get("longitude")!.toString();
+    const radius = body.get("radius")!.toString();
+
+    const update = await prisma.lokasi_tambahan.update({
+      data: {
+        lokasi: lokasi?.toUpperCase(),
+        latitude: latitude,
+        longitude: longitude,
+        radius: radius,
+      },
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!update) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Failed to update lokasi tambahan",
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        status: true,
+        message: "Success to update lokasi tambahan",
+        data: update,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    return handleError(error);
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const authorization = req.headers.get("Authorization");
+
+    const session = await checkSession(authorization);
+    if (!session[0]) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const searchParams = new URL(req.url).searchParams;
+    const menu_url = searchParams.get("menu_url");
+    if (!menu_url) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const roleId = session[1].roleId;
+    const roleAccess = await checkRoles(roleId, menu_url);
+    if (!roleAccess) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const actions = roleAccess?.action ? roleAccess?.action.split(",") : [];
+    if (!actions.includes("delete")) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const id = params.id;
+    if (!id) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Data not found",
+        }),
+        {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const deletes = await prisma.lokasi_tambahan.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!deletes) {
+      return new NextResponse(
+        JSON.stringify({
+          status: false,
+          message: "Failed to delete lokasi tambahan",
+        }),
+        {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        status: true,
+        message: "Success to delete lokasi tambahan",
+        data: deletes,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    return handleError(error);
+  }
+}
